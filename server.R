74bd4a5e679411d0d0a91604b33f9a64026e175f
hliu2020 <51450506+hliu2020@users.noreply.github.com>
2019-07-31 06:29:26 -0400

    Add hover and click functions

--- server.R
+++ server.R
@@ -11,6 +11,7 @@ library(visTree)
 library(randomForest)
 library(caret)
 
+
 source("cluster_method.R")
 nki <- readRDS("breastCancerNKI_200genes.rds") 
 #nki <-  nki[,!duplicated(colnames(nki))]
@@ -30,20 +31,34 @@ shinyServer(function(input, output, session) {
 #create plot  
   boxplot1 <- function(){
     newData <- getData()
-    #    attach(newData)
+        attach(newData)
     gene1<- input$gene
+ #   status <- input$group
     df1 <- newData[,c("er",gene1)]
+    #df1$er <- factor(df1$er)
+    colnames(df1)<-c("er","gene1")
+#    plot.type <- switch(input$plot.type,
+#                      "boxplot" 	= geom_boxplot(y=gene1),
+#                      "histogram" =	geom_histogram(x=gene1,alpha=0.5,position="identity"),
+#                      "density" 	=	geom_density(alpha=.75),
+#                      "bar" 		=	geom_bar(position="dodge"))    
     df1$er <- factor(df1$er)
     colnames(df1)<-c("er","gene1")
-    g <- ggplot(df1, aes(er, gene1)) + geom_boxplot(outlier.colour = "orange", outlier.shape = 1) +
-      labs(title = paste("Boxplot of of the mRNA expression", input$gene, "between ER status"),y=input$gene,x="ER status")
-    
+    g <- ggplot(df1, aes(er,gene1)) + geom_boxplot(outlier.colour = "orange", outlier.shape = 1) + # plot.type +
+      labs(title = paste("Plot of of the mRNA expression", input$gene, "between ER status"),y=input$gene,x="ER status")
   }
   
   output$boxplot1 <- renderPlot({
   	g <- boxplot1()
   	g
-})
+  })
+  # Generate a summary of the data
+  output$summaryT <- renderPrint({
+    newData <- getData()
+    gene1<- input$gene
+    df1 <- newData[,c(gene1)]
+    summary(df1)
+  })
 
 ##----------------Tab 3 cluster--------------------------
 #  selectedData <- reactive({
@@ -78,35 +93,83 @@ shinyServer(function(input, output, session) {
 
 ##---------tab 4- machine learning analysis----------------
 
-#source("ml_nki.R")  
-
-  df <- nki[,c(1:200,202)] %>% mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit() 
-#  df_pred$er <- factor(df_pred$er)
-
-    
+# RenderUI for gene selection
+output$choose_genes <- renderUI({
+   selectInput('bygene', 'Gene symbols', names(nki), multiple=TRUE, selectize=TRUE,
+               selected=c("GREM2","SUHW2"))
+  }) 
   
+getData3 <- reactive({
+    nki %>% subset(select=c("id",input$bygene,"er","os","grade"))
+  })
+
+
+  ###create output tablee of observations    
+  output$table3 <- renderTable({
+       getData3()
+  })
+#------------------Download sutset data used for machine learning---------------
+  output$downloadDataML <- downloadHandler(
+    filename = function() {
+      paste("dataset", ".csv", sep ="")
+    },
+    content = function(file) {
+      write.csv(getData3(), file, row.names = FALSE)
+    },
+    contentType = "csv"
+  )
+ 
+##Subset data for ML analysis 
+#renderUI for gene selection for scatter plot
+output$gene_xaxis <- renderUI({
+    # If missing input, return to avoid error later in function
+    if(is.null(input$bygene))
+      return()
+    
+    # Get the data set with the appropriate name
+    dat <- getData3()  %>% select(-c(os,id,grade))#get(input$bygene)
+    colnames <- names(dat)
+    
+    # Create the checkboxes and select them all by default
+    selectInput("x", "Choose gene for x-axis", 
+                       choices  = colnames,
+                       selected = "er")
+  })
+  output$gene_yaxis <- renderUI({
+    # If missing input, return to avoid error later in function
+    if(is.null(input$bygene))
+      return()
+    
+    # Get the data set with the appropriate name
+    dat <- getData3() %>% select(-c(os,id,grade))#get(input$bygene)
+    colnames <- names(dat)
+    
+    # Create the checkboxes and select them all by default
+    selectInput("y", "Choose gene for y-axis", 
+                choices  = colnames,
+                selected = "SUHW2")
+  })    
+  
+#Split data
+  df <- nki 
   inTrain <- reactive({
     set.seed(123)
     inTrain <- createDataPartition(y = df$er, p = input$size, list = FALSE) 
     inTrain
   })
   
- 
-  
   getFit <- reactive({
+    df <- nki %>% subset(select=c(input$bygene,"er")) %>% mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit()
     df$er <- factor(df$er)
     training <- df[inTrain(),]
-    #  training$er <- factor(training$er, levels=c("0","1"))
-    #  df$er <- factor(df$er)
     testing <- df[-inTrain(),] 
     if(input$z == 'rf'){
               modelFit <- randomForest(er ~., data = training, ntree=500,mtry=input$m)
     } else {modelFit <- rpart(er ~ ., data = training, method="class",control=rpart.control(minsplit=5,cp=0.001))
    modelFit 
    }
-
-
   })
+
   mlplot <-  function() {
   plot(getFit())
   }  
@@ -114,86 +177,69 @@ shinyServer(function(input, output, session) {
     mlplot()
   })
 
-    
+  output$plot_clickinfo <- renderPrint({
+    cat("Click:\n")
+    str(input$plot_click)
+  })
+  output$plot_hoverinfo <- renderPrint({
+    cat("Hover (throttled):\n")
+    str(input$plot_hover)
+  })
+  
+##Prediction    
   output$confusionmatrix <- renderPrint({
-    df_pred <- nki[,c(1:200,202)]
-    df_pred %>%  mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit() -> df
+    df <- nki %>% subset(select=c(input$bygene,"er")) %>% mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit()
+#    df_pred <- nki[,c(1:200,202)]
+#    df_pred %>%  mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit() -> df
      df$er <- factor(df$er)
-     testing <- df[-inTrain(),]
+    testing <- df[-inTrain(),]
     predictions <- predict(getFit(), newdata = testing, type="class")
     
     confusionMatrix(predictions, as.factor(testing$er))
   })
   
   mlplot2 <-  function() {
-    df_pred <- nki[,c(1:200,202)]
-    df_pred %>%  mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit() -> df
+#    df_pred <- nki[,c(1:200,202)]
+#    df_pred %>%  mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit() -> df
+    df <- nki %>% subset(select=c(input$bygene,"er")) %>% mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .)) %>% na.omit()
     df$er <- factor(df$er)
     testing <- df[-inTrain(),]
     predictions <- predict(getFit(), newdata = testing)
     testing$predRight <- ifelse(predictions == as.factor(testing$er),1,0)
-    ggplot(data = testing, aes_string(x = input$x , y = input$y)) + aes(color = predRight) + geom_point(lwd = 4) +theme(text = element_text(size=20))
+    ggplot(data = testing, aes_string(x = input$x , y = input$y)) + aes(color = predRight) +
+    geom_point(lwd = 4) +theme(text = element_text(size=20))
 
-  }  
+  }
+  
   output$mlplot2 <- renderPlot({
     mlplot2()
   }) 
+output$ex1 <- renderUI({
+      withMathJax(
+      helpText('The busy Cauchy distribution
+               $$\\frac{1}{\\pi\\gamma\\,\\left[1 +
+               \\left(\\frac{x-x_0}{\\gamma}\\right)^2\\right]}\\!$$'))
+})
 
+ 
   
-##--------- tab5 data subset export--------------------  
-  #update title info
-  output$title1 <- renderText({
-    paste0("Summary of the mRNA expression of", input$gene)
-      })  
-###create output for summary data    
-  output$summaryT <- renderPrint({
-    dt1 <- getData()
-    g1 <- input$gene
-    dt1 %>% subset(select=c(input$gene)) %>% summary()
-    #tapply(dt$g1, dt$er, summary())
-  })
-  
-#  observe({
-#    if(input$os){
-#    updateSliderInput(session,"size",max=10, min = 3)
-#    } else {
-#    updateSliderInput(session, "size", min = 1, max = 10)
-#  }
-#    })
-###create output tablee of observations    
-  output$table <- renderTable({
-    dt2 <- getData2()
-
-  })
-  
-  
-#------------------Download data---------------
-  output$downloadData <- downloadHandler(
+  #------------------Download boxplot ---------------  
+  output$downloadPlot <- downloadHandler(
     filename = function() {
-      paste("dataset", ".csv", sep ="")
+      paste("boxplot", '.png', sep='')
     },
     content = function(file) {
-      write.csv(getData2(), file, row.names = FALSE)
-    },
-    contentType = "csv"
+      ggsave(file, plot = boxplot1(), device = "png")
+    }
   )
-
-#------------------Download boxplot ---------------  
-   output$downloadPlot <- downloadHandler(
-          filename = function() {
-              paste("boxplot", '.png', sep='')
-          },
-          content = function(file) {
-              ggsave(file, plot = boxplot1(), device = "png")
-          }
-      )
-#------------------Download plot 2---------------  
+  
+#------------------Download cluster plot ---------------  
   output$downloadPlot2 <- downloadHandler(
     filename = function() {
       paste("cluster", '.png', sep='')
     },
     content = function(file) {
-      ggsave(file, plot =   clusterplot1(), device = "png")
+      ggsave(file, plot = clusterplot1(), device = "png")
     }
   )  
   
@@ -202,7 +248,19 @@ shinyServer(function(input, output, session) {
       paste("cluster", '.png', sep='')
     },
     content = function(file) {
-      ggsave(file, plot =   pariplot(), device = "png")
+      ggsave(file, plot = pariplot(), device = "png")
+    }
+  )  
+ 
+#------------Download ML plot--------------  
+
+  
+  output$downloadPlot4 <- downloadHandler(
+    filename = function() {
+      paste("mlplot2", '.png', sep='')
+    },
+    content = function(file) {
+      ggsave(file, plot=mlplot2(), h=6, w=6, units="in",dpi=300)#not work for mlplot
     }
   )  
   
